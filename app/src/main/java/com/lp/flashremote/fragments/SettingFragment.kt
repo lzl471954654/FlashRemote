2622cfdb7aaede9313a286c6bcca5c2774460dea
LZL <471954654@qq.com>
2017-09-11 23:20:49 +0800

    设置bug修复

--- app/src/main/java/com/lp/flashremote/fragments/SettingFragment.kt
+++ app/src/main/java/com/lp/flashremote/fragments/SettingFragment.kt
@@ -1,15 +1,25 @@
 package com.lp.flashremote.fragments
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
+import android.os.Environment
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.lp.flashremote.R
+import com.lp.flashremote.activities.FileExplorerActivity
+import com.lp.flashremote.adapters.FIleExplorerAdapter
+import com.lp.flashremote.adapters.getSizeText
 import kotlinx.android.synthetic.main.fragment_setting.view.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.support.v4.startActivity
+import org.jetbrains.anko.uiThread
+import java.io.File
 
 /**
  * Created by PUJW on 2017/8/14.
@@ -18,6 +28,7 @@ import kotlinx.android.synthetic.main.fragment_setting.view.*
 class SettingFragment : Fragment(),View.OnClickListener {
     lateinit var viewArray:Array<View>
     lateinit var root:View
+    var cacheSize:Long = 0L
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         root =  inflater!!.inflate(R.layout.fragment_setting, null)
         return root
@@ -26,6 +37,32 @@ class SettingFragment : Fragment(),View.OnClickListener {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         viewArray  = arrayOf(root.settings_about_us,root.settings_clear_cache,root.settings_clear_folder,root.settings_feedback,root.settings_see_accept_folder)
         viewArray.forEach { it.setOnClickListener(this) }
+        Log.i("dir:",context.filesDir.absolutePath)
+        Log.i("size:",context.filesDir.length().toString())
+        //refreshCacheSize()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("resume")
+        refreshCacheSize()
+    }
+
+    fun refreshCacheSize(){
+        doAsync {
+            fun countSize(parent:File):Long{
+               var size = 0L
+                parent.listFiles().forEach {
+                    size += if(it.isDirectory)
+                        countSize(it)
+                    else
+                        it.length()
+                }
+                return size
+            }
+            cacheSize = countSize(context.filesDir.parentFile)
+            uiThread { root.settings_cache_size.text = getSizeText(cacheSize) }
+        }
     }
 
     override fun onClick(v: View?) {
@@ -43,15 +80,15 @@ class SettingFragment : Fragment(),View.OnClickListener {
 
             }
             R.id.settings_see_accept_folder->{
-
+                startActivity<FileExplorerActivity>("mode" to FileExplorerActivity.MODE_EXPLORER,"dataType" to FIleExplorerAdapter.BASE_FILE_EXPLORER,"title" to "目录浏览","rootPath" to context.filesDir.parentFile.absolutePath)
             }
             R.id.settings_exit->{
                 val dialog = AlertDialog.Builder(context)
                 dialog.setMessage("你要走吗")
-                dialog.setNegativeButton("再见",object : DialogInterface.OnClickListener{
-                    override fun onClick(p0: DialogInterface?, p1: Int) {
-                    }
-                })
+                dialog.setNegativeButton("再见") {
+                    p0, p1 ->
+
+                }
             }
         }
     }
