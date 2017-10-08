be110456f7c42b88fdea6804e5f09fe6357f6b36
wangjiapu <2405491487@qq.com>
2017-10-08 21:28:22 +0800

    鼠标

--- app/src/main/java/com/lp/flashremote/utils/Command2JsonUtil.java
+++ app/src/main/java/com/lp/flashremote/utils/Command2JsonUtil.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.lp.flashremote.beans.Command;
 import com.lp.flashremote.beans.MouseOpInfo;
 
+import java.util.Map;
+
 /**
  * Created by PUJW on 2017/9/15.
  */
@@ -17,11 +19,12 @@ public class Command2JsonUtil {
         return new Gson().toJson(command);
     }
 
-    public static String getMouseJson(float x,float y,boolean click,boolean single,
+    public static String getMouseJson(Map<Integer,Integer> map, boolean click, boolean single,
                                       boolean doubleClick, boolean right){
         MouseOpInfo mouse=new MouseOpInfo();
-        mouse.setX(x);
-        mouse.setY(y);
+      /*  mouse.setX(x);
+        mouse.setY(y);*/
+         mouse.setMap(map);
         mouse.setClick(click);
         mouse.setSingleClick(single);
         mouse.setDoubleClick(doubleClick);
