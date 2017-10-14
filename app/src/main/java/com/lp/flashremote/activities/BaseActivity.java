09c0d6a4617e431a099d2354d24998343e8249e6
LZL <471954654@qq.com>
2017-10-14 16:52:29 +0800

    修改Activity突然消失问题 麻瓜蒲家旺 在onPause()方法里面写了个finish()

--- app/src/main/java/com/lp/flashremote/activities/BaseActivity.java
+++ app/src/main/java/com/lp/flashremote/activities/BaseActivity.java
@@ -1,12 +1,17 @@
 package com.lp.flashremote.activities;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import com.lp.flashremote.R;
@@ -24,8 +29,17 @@ import permissions.dispatcher.RuntimePermissions;
  * Created by xiyou3g on 2017/10/6.
  *
  */
-@RuntimePermissions
+//@RuntimePermissions
 public class BaseActivity extends AppCompatActivity {
+
+    String[] permissions = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.CHANGE_NETWORK_STATE
+            //Manifest.permission.WRITE_SETTINGS
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,17 +48,18 @@ public class BaseActivity extends AppCompatActivity {
         setContentView(R.layout.acticity_base);
         PropertiesUtil.getProperties(this);
         SharePerferenceUtil.getUserInfo(this);
-
-        BaseActivityPermissionsDispatcher.grantPermissionWithPermissionCheck(this);
+        getPermission();
+        //BaseActivityPermissionsDispatcher.grantPermissionWithPermissionCheck(this);
     }
-    @NeedsPermission({
+    /*@NeedsPermission({
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA,
             Manifest.permission.RECORD_AUDIO,
             Manifest.permission.CHANGE_WIFI_STATE,
-            Manifest.permission.CHANGE_NETWORK_STATE
-    })
+            Manifest.permission.CHANGE_NETWORK_STATE,
+            Manifest.permission.WRITE_SETTINGS
+    })*/
     protected void grantPermission() {
         System.out.println("grantPermission");
         Timer timer=new Timer();
@@ -58,31 +73,120 @@ public class BaseActivity extends AppCompatActivity {
     }
 
 
-    @OnPermissionDenied({
+    /*@OnPermissionDenied({
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA,
             Manifest.permission.RECORD_AUDIO,
             Manifest.permission.CHANGE_WIFI_STATE,
-
-            Manifest.permission.CHANGE_NETWORK_STATE
-    })
+            Manifest.permission.CHANGE_NETWORK_STATE,
+            Manifest.permission.WRITE_SETTINGS
+    })*/
     void showDenied() {
         finish(); // 如果用户拒绝该权限执行的方法
     }
 
-    @Override
+    /*@Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         BaseActivityPermissionsDispatcher.onRequestPermissionsResult(this,requestCode,grantResults);
+    }*/
+
+
+    private boolean checkePermissionAllGet(String[] permissions){
+        boolean flag = true;
+        for (String s : permissions) {
+            if(ContextCompat.checkSelfPermission(this,s)== PackageManager.PERMISSION_DENIED){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
     }
 
+    private void getPermission(){
+        boolean isAllGet = checkePermissionAllGet(permissions);
+        if(!isAllGet){
+            ActivityCompat.requestPermissions(this,permissions,100);
+        }else
+        {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Intent intent=new Intent(BaseActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).start();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==100){
+            boolean flag = true;
+            for (int result : grantResults) {
+                if(result == PackageManager.PERMISSION_DENIED){
+                    flag = false;
+                    break;
+                }
+            }
+            if(!flag){
+                openAppDetails();
+            }else {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(1500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        Intent intent=new Intent(BaseActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }).start();
+            }
+        }
+    }
+
+    private void openAppDetails() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("本应用需要文件读写，网络状态改变，修改系统设置，录音权限，否则无法正常使用应用，请到 “应用信息 -> 权限” 中授予！");
+        builder.setPositiveButton("去手动授权", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                startActivity(intent);
+                finish();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.show();
+    }
 
 
     @Override
     protected void onPause() {
         super.onPause();
-        finish();
+        //finish();
     }
 }
